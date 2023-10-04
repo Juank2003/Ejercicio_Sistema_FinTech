35d205f4fbfc9bf1c7217fc8018ccc456794e77b
Juank003 <jortealv@myuax.com>
2023-10-04 20:45:48 +0200

    ClaseHiloCliente

--- src/main/java/org/example/HiloCliente.java
+++ src/main/java/org/example/HiloCliente.java
@@ -1,4 +1,25 @@
 package org.example;
 
-public class HiloCliente {
+public class HiloCliente  implements Runnable{
+
+    private Cuenta cuenta;
+    private double cantidad; //la cantidad de saldo del deposito y retiro
+
+    //Constructor
+    public HiloCliente(Cuenta cuenta, double cantidad) {
+        this.cuenta = cuenta;
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public void run() {
+        if (cantidad>0){
+            cuenta.depositar(cantidad);
+
+        }else{
+            cuenta.retirar(cantidad);
+
+        }
+
+    }
 }
